96346df308a86d25af44bcdf06d09f4f3ee29128
isorokina <93764917+isorokina@users.noreply.github.com>
2022-09-14 15:46:18 +0300

    Update laukumi.py

--- laukumi.py
+++ laukumi.py
@@ -1,10 +1,17 @@
 from math import*
+from xml.etree.ElementTree import PI
 class Prizmas():
     H=12
-  
-  
+   
     def pamataLaukums(self,n,a):
-        S=(n*a*a)/(4*tan(180/n))
+        return (n*a*a)/(4*tan(3/n))
 
+
+
+
+print(Prizmas.H)
 atr=Prizmas()
-print(atr.pamataLaukums(4,12))
+print(atr.pamataLaukums(6,2))
+atr1=atr.pamataLaukums(6,2)
+print(atr1)
+
